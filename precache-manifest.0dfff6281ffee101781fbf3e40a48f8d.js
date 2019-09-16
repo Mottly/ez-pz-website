ce45901fe836575aaf4e5f340da879f023ff3fea
Mottly <55404339+Mottly@users.noreply.github.com>
2019-09-16 12:57:31 -0400

    Updates

---
+++ precache-manifest.0dfff6281ffee101781fbf3e40a48f8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad948b478cacd3a5203c2b94538ed47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc3a145273bb4dc1202d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ee0f2b2a3b967cebe2f7",
+    "url": "/static/js/2.ef6aaae7.chunk.js"
+  },
+  {
+    "revision": "bc3a145273bb4dc1202d",
+    "url": "/static/js/main.f4659199.chunk.js"
+  },
+  {
+    "revision": "a2d0669af8e3b12ff9d4",
+    "url": "/static/js/runtime~main.9b048c6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
